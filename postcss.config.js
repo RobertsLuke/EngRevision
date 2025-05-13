fd1ea10017b43e7d4521d20be222cfaf9d7d1cb7
robertsluke <lukearthurroberts@gmail.com>
2025-05-13 19:22:34 +0100

    Fix PostCSS config to use ES Module syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+// Export using ES modules syntax since the project has "type": "module" in package.json
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
--- src/node-polyfills.js
+++ src/node-polyfills.js
@@ -1,6 +1,7 @@
 // node-polyfills.js
 // This file creates polyfills for Node.js built-in modules used in the browser
 
+// Note: Using the full Buffer import since we're in an ES module
 import { Buffer } from 'buffer';
 
 // Add Node.js global objects to the window object
